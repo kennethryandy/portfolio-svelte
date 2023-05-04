fd39f0b08ebf395bdfacfe7d0cc2cf963a825210
Kenneth Ryan <dykennethryan@gmail.com>
2023-05-04 23:18:38 +0800

    Initialize sveltekit

---
+++ src/lib/config.ts
@@ -0,0 +1,7 @@
+import { dev } from '$app/environment';
+
+export const title = 'Kenneth Ryan Dy / Web Developer';
+export const description =
+	'Kenneth Ryan Dy is a web developer specializing in building exceptional digital experiences.';
+export const url = dev ? 'http://127.0.0.1:5173/' : 'https://kennethryandy.vercel.app/';
+export const author = '@kennethryandy';
